035dc50aa5c0012bc4641e162b0563aa865629e1
종근 오 <kyunggioh@hanmail.net>
2020-07-02 16:58:17 +0900

    .

--- CS_Homework/CS_Homework/Program.cs
+++ CS_Homework/CS_Homework/Program.cs
@@ -13,9 +13,68 @@ namespace CS_Homework
         static void Main(string[] args)
         {
 
-
         }
     }
 
-    
+    class Orc
+    {
+        protected int hp;
+        protected int att;
+
+        public void Move() { }
+    }
+    class GroundOrc : Orc
+    {
+
+        public void Attack() { }
+        public void Patrol() { }
+    }    
+    class GroundMagicalOrc : GroundOrc
+    {
+        private int mp;
+        private int masteryGrade;
+    }
+    class Grunt : GroundOrc
+    {
+        private int weaponRange;
+        private bool isAttackFlying;
+
+    }
+    class Raider : GroundOrc
+    {
+        private int weaponRange;
+        private bool isAttackFlying;
+
+        public void Ensnare() { }
+    }
+    class Shamen : GroundMagicalOrc
+    {
+        private int mpRegen;
+        public void Purge() { }
+        public void LighteningShield() { }
+        public void BloodLust() { }
+
+    }
+    class TrollWitchDoctor : GroundMagicalOrc
+    {
+        private int hpRegen;
+        public void CentryWard() { }
+        public void StasisTrap() { }
+        public void HealingWard() { }
+    }
+    class FlyingOrc : Orc
+    {
+        public void Fly() { }
+    }
+    class WindRider : FlyingOrc
+    {
+        private int venomOverlap;
+        void EnvenomedSpear() { }
+    }
+    class TrollBatRider : FlyingOrc
+    {
+        private int accelConstant;
+        void LiquidFire() { }
+        void UnstableConcoction() { }
+    }
 }
